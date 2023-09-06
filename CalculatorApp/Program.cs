6aaeffab1f6f2eb171b7094dbd4054a0a32ceea1
Starinin Andrey <starininandrey@gmail.com>
2023-09-06 21:08:26 +0300

    Update

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalculatorApp;
+
+//var calculator = new Calculator(ConsoleLog.PrintInfo);
+var file = new FileLog()
+{
+    Path = "calc.log"
+};
+//var calculator = new Calculator(file.WriteToFile);
+
+var log = new Action<string>(file.WriteToFile);
+log += ConsoleLog.PrintInfo;
+
+var calculator = new Calculator(log);
+calculator.Add(5, 10);
+calculator.Sub(5, 10);
+calculator.Mul(5, 10);
+calculator.Div(5, 10);
